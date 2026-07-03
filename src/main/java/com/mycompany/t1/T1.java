08a48c08ff849d16cc3b73c2dbc1f25ef61bd949
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-03 11:59:55 +0800

    upload

---
+++ src/main/java/com/mycompany/t1/T1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t1;
+
+/**
+ *
+ * @author limis
+ */
+public class T1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
