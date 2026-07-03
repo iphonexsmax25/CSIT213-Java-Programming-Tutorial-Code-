3c80277bb5095d33438150e1d08270e524c098f2
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-03 15:47:44 +0800

    create test file

---
+++ src/main/java/com/mycompany/t1/Test.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t1;
+
+/**
+ *
+ * @author limis
+ */
+public class Test {
+    public static void main(String[] argv){
+        testItem();
+        testArray();
+        testInventory();
+    }
+    public static void testItem(){
+        
+    }
+    public static void testArray(){
+        
+    }
+      public static void testInventory(){
+        
+    }
+}
