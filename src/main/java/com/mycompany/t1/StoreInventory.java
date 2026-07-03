84c40d031fd9343471c5af5fddf30cda1cd2fca6
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-03 16:46:40 +0800

    Store Inventory class

--- src/main/java/com/mycompany/t1/StoreInventory.java
+++ src/main/java/com/mycompany/t1/StoreInventory.java
@@ -9,5 +9,21 @@ package com.mycompany.t1;
  * @author limis
  */
 public class StoreInventory {
+    private String branch;
     
+    public StoreInventory(String bName){
+        branch = bName;
+    }
+    public String getBranch(){
+        return branch;
+    }
+    public boolean addItem(StoreItem anItem){
+        return false;
+    }
+    public boolean delItem(String itemID){
+        return false;
+    }
+    public String toString(){
+        return "";
+    }
 }
