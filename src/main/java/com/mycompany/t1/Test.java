9ddc8a99ccab9661e828af6d78c0fb31ac5c5efe
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-03 16:29:31 +0800

    for loop

--- src/main/java/com/mycompany/t1/Test.java
+++ src/main/java/com/mycompany/t1/Test.java
@@ -28,7 +28,7 @@ public class Test {
         items[0]= new StoreItem("item 1", "name 1",1,100);
         items[0]= new StoreItem("item 3", "name 3",3,300);
         for(int i = 0; i< items.length; i++){
-            
+            System.out.println(items[i]);
         }
     }
     public static void testInventory(){
