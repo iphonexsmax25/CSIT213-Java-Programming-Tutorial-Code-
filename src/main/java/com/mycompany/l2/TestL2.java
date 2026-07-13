08b9b4866c330a6880d3e2752a2fb51650b3eca1
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-13 16:48:06 +0800

    works

--- src/main/java/com/mycompany/l2/TestL2.java
+++ src/main/java/com/mycompany/l2/TestL2.java
@@ -8,9 +8,20 @@ package com.mycompany.l2;
  *
  * @author limis
  */
+import java.util.ArrayList;
 public class TestL2 {
      public static void main(String[] args){
-         testItem();
+         // testItem();
+         testArrayList();
+     }
+     public static void testArrayList(){
+         ArrayList<ShipmentItem> sItems = new ArrayList<>();
+         sItems.add(new ShipmentItem("p1", 100));
+         sItems.add(new ShipmentItem("p2", 200));
+         for ( int i = 0; i< sItems.size(); i++){
+             System.out.println(sItems.get(i));
+         }
+         
      }
      public static void testItem(){
          ShipmentItem  sItem1 = new ShipmentItem("p1", 100);
@@ -21,9 +32,13 @@ public class TestL2 {
          System.out.println(sItem2);
          System.out.println(sItem1.equals(sItem2));
          System.out.println(sItem2.equals(sItem3));
+         
+         
          //REquest Item
          RequestItem rItem1 = new RequestItem("p1", 50);
          RequestItem rItem2 = new RequestItem("p2", 500);
+         
+         
          System.out.println(rItem1);
          System.out.println(rItem2);
          System.out.println(rItem1.equals(rItem2));
