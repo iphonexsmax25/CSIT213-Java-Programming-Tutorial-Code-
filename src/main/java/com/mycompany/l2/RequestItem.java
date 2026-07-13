8987d7ecba0a530fce84304bf27c251e11875c78
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-13 16:13:38 +0800

    works

--- src/main/java/com/mycompany/l2/RequestItem.java
+++ src/main/java/com/mycompany/l2/RequestItem.java
@@ -9,5 +9,28 @@ package com.mycompany.l2;
  * @author limis
  */
 public class RequestItem {
+    private String code;
+    private int qtyRequested;
+    private int qtySupplied;
     
+    public RequestItem(String xCode, int rQty){
+        code = xCode;
+        qtyRequested = rQty;
+    }
+    public String getCode(){
+        return code;
+    }
+    public int getQtyRequested(){
+        return qtyRequested;
+    }
+    public void setQtyRequested(int sQty){
+        if(sQty >= 0){
+            qtySupplied = sQty;
+        }
+    }
+    public String toString(){
+        return String.format("", code, qtyRequested, qtySupplied);
+    }
+    
+            
 }
