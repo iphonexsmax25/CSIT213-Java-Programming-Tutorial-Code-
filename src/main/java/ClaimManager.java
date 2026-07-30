bf2e548915701d28de47e040cdfca21d948e663b
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-30 17:26:23 +0800

    works

--- src/main/java/ClaimManager.java
+++ src/main/java/ClaimManager.java
@@ -12,9 +12,23 @@ public class ClaimManager {
     }
     public void load(){
         claims.add(new Claim("e1", LocalDate.of(2026, 4, 25)));
+          claims.add(new ClaimTpt("e1", LocalDate.of(2026, 6, 30), 200));
+        
         claims.add(new ClaimTpt("e2", LocalDate.of(2026, 6, 30), 200));
-        claims.add(new ClaimOT("e3", LocalDate.of(2026, 7, 30), 10));
+        claims.add(new ClaimOT("e2", LocalDate.of(2026, 7, 30), 10));
+        
+        claims.add(new ClaimOT("e3", LocalDate.of(2026, 7, 30), 1));
+        claims.add(new ClaimOT("e3", LocalDate.of(2026, 7, 25), 2));
+        claims.add(new Claim("e3", LocalDate.of(2026, 4, 25)));
+        
         claims.add(new ClaimOT("e4", LocalDate.of(2026, 7, 30), 2));
+    }
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        for (Claim c : claims){
+            result.append(c.toString() + "\n");
+        }
+        return result.toString();
     }
     
 }
