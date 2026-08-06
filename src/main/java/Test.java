82c0cc1b3297685e645ae977c743dc48e7c154e8
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-06 16:36:27 +0800

    works

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,8 +9,17 @@ public class Test {
         
     }
     public static void test2(){
-        
-        
+        ArrayList<Payable> items = new ArrayList<>();
+        EquipRental r = new EquipRental("r1", "desc 1", LocalDate.of(2026, 3, 20), 50, 10);
+        Subscription s = new Subscription ("s1", "name 1", LocalDate.of(2025, 12, 1), LocalDate.of(2026, 12, 1), 100);
+        PremiumSubscription p = new PremiumSubscription("p1", "name 1", LocalDate.of(2025, 12, 1), LocalDate.of(2026, 12, 1), 100, 0.2);
+        // Add to ArrayList
+        items.add(r);
+        items.add(s);
+        items.add(p);
+        for (Payable item : items){
+            System.out.println(item);
+        }
     }
     public static void test1(){
         EquipRental r = new EquipRental("r1", "desc 1", LocalDate.of(2026, 3, 20), 50, 10);
