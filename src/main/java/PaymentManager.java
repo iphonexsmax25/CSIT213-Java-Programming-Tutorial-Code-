9478bdeee41190e45f0160538f5838e66cb44827
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-06 17:03:28 +0800

    works

--- src/main/java/PaymentManager.java
+++ src/main/java/PaymentManager.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.format.DateTimeFormatter;
 
 
 
 
 public class PaymentManager {
+    private ArrayList<Payable> items;
+    public PaymentManager(){
+        items = new ArrayList<>();
+    }
     
+    public void load (String filename){
+        
+    }
+    
+    public double getTotalAmount(){
+        double total = 0;
+        return total;
+    }
+    
+    public double getTotalSuncription(){
+        double total = 0; 
+        return total;
+    }
 }
