9883392db217ccadaf6e8cb494e82fc06f058eb1
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-06 15:47:26 +0800

    works

--- src/main/java/EquipRental.java
+++ src/main/java/EquipRental.java
@@ -1,12 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.time.LocalDate;
 
-/**
- *
- * @author limis
- */
+
+
+
 public class EquipRental {
+    private String code, description;
+    private LocalDate dateRented;
+    private int qty;
+    private double cost;
+    public EquipRental(String code, String description, LocalDate dateRented,
+            int qty, double cost){
+        
+        this.code = code;
+        this.description = description; 
+        this.dateRented = dateRented;
+        this.qty = qty;
+        this.cost = cost;
+    }
     
+    public double getAmount(){
+        return qty * cost;
+    }
+    public String toString(){
+        return String.format(" %s %s $%.2f",code, dateRented, getAmount());
+    }
+    public boolean equals(Object other){
+        if (other == null)
+            return false;
+        if(other instanceof EquipRental)
+            return false;
+        EquipRental r = (EquipRental)other;
+        return code.equals(r.code);
+    }
 }
