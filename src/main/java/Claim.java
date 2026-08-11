d746297dde7a64465cf3987d6d679a23e9d2f650
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-11 13:23:23 +0800

    works

--- src/main/java/Claim.java
+++ src/main/java/Claim.java
@@ -4,6 +4,19 @@ import java.time.LocalDate;
 public class Claim {
     private String empId;
     private  LocalDate dateOfClaim;
-    private
+    public Claim(String empId, LocalDate dateofClaim){
+        this.empId = empId;
+        this.dateOfClaim = dateOfClaim;
+    }
+    
+    public String getEmpId(){
+        return empId;
+    }
+    
+    public int getYearOfClaim(){
+        return dateOfClaim.getYear();
+    }
+    
+    
     
 }
