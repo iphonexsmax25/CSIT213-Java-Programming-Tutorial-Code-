d5b227b14765558f07a69cecf84ddf42872d80dd
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-13 13:42:21 +0800

    works

--- src/main/java/ClaimOT.java
+++ src/main/java/ClaimOT.java
@@ -9,7 +9,23 @@ public class ClaimOT extends Claim {
         super(empId, dateOfClaim);
         hoursOT = hours;
     }
-    
+     public int getHoursOT(){
+         return hoursOT;
+     }
+     
+     @Override
+     public double getClaimAmount(){
+         double totalOT = hoursOT * CLAIM_PER_HOUR;
+         if (totalOT > CLAIM_MAX){
+             return CLAIM_MAX;
+         }
+         else{
+             return totalOT;
+         }
+     }
+     public String toString(){
+         return String.format("ClaimOT Hour(s): %d %s", hoursOT, super.toString());
+     }
     
     
 }
